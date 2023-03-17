36f422572be983eb88198b5836a00ec0ca5e7cb7
SergioAlmagre <127857356+SergioAlmagre@users.noreply.github.com>
2023-03-17 19:42:36 +0100

    descomentado mitad del main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -21,51 +21,51 @@ fun main(args: Array<String>) {
     println(campoBatalla)
 
 
-//    while(tiempo <= 60){
-//
-//        if (tiempo % 3 == 0){//Se mueven las tropas.
-//            println("Las tropas se mueven")
-//            for (i in 0..campoBatalla.getCantFilas()-1){
-//                for (j in 0 .. campoBatalla.getCantCols()-1){
-//                    if (campoBatalla.getCampo(i,j) is Legion){
-//                        println("Se mueve la legión ${campoBatalla.getCampo(i,j)}")
-//                        var movida = false
-//                        var intentosMover = 1
-//                        while(!movida && intentosMover < 6){
-//                            var posMover = eligePosValida(i,j, campoBatalla)
-//                            if (campoBatalla.getCampo(posMover[0],posMover[1])==null){//Si el lugar elegido está en blanco
-//                                campoBatalla.setCampo(posMover[0],posMover[1], campoBatalla.getCampo(i, j))  //La tropa se desplaza
-//                                campoBatalla.setCampo(i, j, null) //Donde estaba se queda a nulo
-//                                movida = true
-//                            } else {//Si no es nulo veremos si es una legión o un puente
-//                                if (campoBatalla.getCampo(posMover[0],posMover[1]) is Legion) { //Comprobamos si es una legión contrincante.
-//                                    var legionActual = campoBatalla.getCampo(i, j) as Legion
-//                                    var legionContraria = campoBatalla.getCampo(posMover[0],posMover[1]) as Legion
-//                                    if (legionActual.getNombre().substring(0,1) != legionContraria.getNombre().substring(0,1)){ //Son legiones contrarias.
-//                                        println("Las legiones ${legionActual.getNombre()} y ${legionContraria.getNombre()} luchan por la posición ${Arrays.toString(posMover)}")
-//                                        if (luchanLegions(legionActual, legionContraria) == 1){//Gana la que se movía y por lo tanto ocupa la posición de la otra.
-//                                            println("Gana la legión ${legionActual.getNombre()}")
-//                                            campoBatalla.setCampo(posMover[0],posMover[1], campoBatalla.getCampo(i, j))  //La tropa se desplaza y machaca a la que había antes ahí, porque le ha ganado la batalla.
-//                                            campoBatalla.setCampo(i, j, null) //Donde estaba se queda a nulo
-//                                        }
-//                                        else {//Ha perdido la que se desplazaba y por lo tanto esa posición se pone a null.
-//                                            println("Gana la legión ${legionContraria.getNombre()}")
-//                                            campoBatalla.setCampo(i, j, null) //Donde estaba se queda a nulo, porque ha perdido.
-//                                        }
-//                                        movida = true
-//                                    }
-//                                }
-//                                if (campoBatalla.getCampo(posMover[0],posMover[1]) is String){  //Comprobamos si es un río
-//                                    var cad = campoBatalla.getCampo(posMover[0],posMover[1]) as String
-//                                    if (cad.equals("R")){
-//                                        var posCruce = cruzaRio(i, j)
-//                                        //El bloque que sigue está copiado y pegado del anterior cambiando posMover por posCruce. Esto es una posible solución. Se podría encapsular en una función.
-//                                        if (campoBatalla.getCampo(posCruce[0],posCruce[1])==null){//Si el lugar elegido está en blanco
-//                                            campoBatalla.setCampo(posCruce[0],posCruce[1], campoBatalla.getCampo(i, j))  //La tropa se desplaza
-//                                            campoBatalla.setCampo(i, j, null) //Donde estaba se queda a nulo
-//                                            movida = true
-//                                        }
-//                                        else {
+    while(tiempo <= 60){
+
+        if (tiempo % 3 == 0){//Se mueven las tropas.
+            println("Las tropas se mueven")
+            for (i in 0..campoBatalla.getCantFilas()-1){
+                for (j in 0 .. campoBatalla.getCantCols()-1){
+                    if (campoBatalla.getCampo(i,j) is Legion){
+                        println("Se mueve la legión ${campoBatalla.getCampo(i,j)}")
+                        var movida = false
+                        var intentosMover = 1
+                        while(!movida && intentosMover < 6){
+                            var posMover = eligePosValida(i,j, campoBatalla)
+                            if (campoBatalla.getCampo(posMover[0],posMover[1])==null){//Si el lugar elegido está en blanco
+                                campoBatalla.setCampo(posMover[0],posMover[1], campoBatalla.getCampo(i, j))  //La tropa se desplaza
+                                campoBatalla.setCampo(i, j, null) //Donde estaba se queda a nulo
+                                movida = true
+                            } else {//Si no es nulo veremos si es una legión o un puente
+                                if (campoBatalla.getCampo(posMover[0],posMover[1]) is Legion) { //Comprobamos si es una legión contrincante.
+                                    var legionActual = campoBatalla.getCampo(i, j) as Legion
+                                    var legionContraria = campoBatalla.getCampo(posMover[0],posMover[1]) as Legion
+                                    if (legionActual.getNombre().substring(0,1) != legionContraria.getNombre().substring(0,1)){ //Son legiones contrarias.
+                                        println("Las legiones ${legionActual.getNombre()} y ${legionContraria.getNombre()} luchan por la posición ${Arrays.toString(posMover)}")
+                                        if (luchanLegions(legionActual, legionContraria) == 1){//Gana la que se movía y por lo tanto ocupa la posición de la otra.
+                                            println("Gana la legión ${legionActual.getNombre()}")
+                                            campoBatalla.setCampo(posMover[0],posMover[1], campoBatalla.getCampo(i, j))  //La tropa se desplaza y machaca a la que había antes ahí, porque le ha ganado la batalla.
+                                            campoBatalla.setCampo(i, j, null) //Donde estaba se queda a nulo
+                                        }
+                                        else {//Ha perdido la que se desplazaba y por lo tanto esa posición se pone a null.
+                                            println("Gana la legión ${legionContraria.getNombre()}")
+                                            campoBatalla.setCampo(i, j, null) //Donde estaba se queda a nulo, porque ha perdido.
+                                        }
+                                        movida = true
+                                    }
+                                }
+                                if (campoBatalla.getCampo(posMover[0],posMover[1]) is String){  //Comprobamos si es un río
+                                    var cad = campoBatalla.getCampo(posMover[0],posMover[1]) as String
+                                    if (cad.equals("R")){
+                                        var posCruce = cruzaRio(i, j)
+                                        //El bloque que sigue está copiado y pegado del anterior cambiando posMover por posCruce. Esto es una posible solución. Se podría encapsular en una función.
+                                        if (campoBatalla.getCampo(posCruce[0],posCruce[1])==null){//Si el lugar elegido está en blanco
+                                            campoBatalla.setCampo(posCruce[0],posCruce[1], campoBatalla.getCampo(i, j))  //La tropa se desplaza
+                                            campoBatalla.setCampo(i, j, null) //Donde estaba se queda a nulo
+                                            movida = true
+                                        }
+                                        else {
 //                                            if (campoBatalla.getCampo(posCruce[0],posCruce[1]) is Legion) {
 //                                                var legionActual = campoBatalla.getCampo(i, j) as Legion
 //                                                var legionContraria = campoBatalla.getCampo(posCruce[0],posCruce[1]) as Legion
